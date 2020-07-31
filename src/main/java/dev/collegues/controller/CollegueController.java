14f285fb676988411b6db53f43f947a43c30a9e2
Antoine LABEEUW <antoine.labeeuw@gmail.com>
2020-07-31 14:47:38 +0200

    connection pool fix + @CrossOrigin on controller

--- src/main/java/dev/collegues/controller/CollegueController.java
+++ src/main/java/dev/collegues/controller/CollegueController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,6 +26,7 @@ import dev.collegues.service.CollegueService;
  * @author antoinelabeeuw
  *
  */
+@CrossOrigin
 @RestController
 @RequestMapping("collegues")
 public class CollegueController {
